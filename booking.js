4f5adbe05390a3eeaec72f5fc28a6cc74e57b099
galathe <galathe.pro@yahoo.com>
2024-03-19 16:42:55 +0100

    nojquery

--- booking.js
+++ booking.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
+    // Cette fonction est déclenchée lorsque le document est prêt
     $('.pop-up').on('click', function(){
+        // Cette fonction est déclenchée lorsque l'utilisateur clique sur l'icône de calendrier
         $('#overlay').fadeIn(300); 
         $('.calendar').fadeIn(300); 
         let clickedbutton = $("input",$(this).parent()).attr('id');
@@ -9,12 +11,14 @@ $(document).ready(function(){
     $('table').on('click', function(event){
       let that=$(event.target);
        if(that.is('td') && !that.hasClass('notCurMonth') && !that.hasClass('holiday') && !that.hasClass('curDay')){
+           // Cette fonction est déclenchée lorsque l'utilisateur clique sur une cellule du calendrier
            $('td.curDay').toggleClass('curDay');
            that.toggleClass('curDay');
        }
    }); 
    
    $('#add_event').on('click', function(){
+       // Cette fonction est déclenchée lorsque l'utilisateur clique sur le bouton "Add event"
        let value= $('td.curDay').html();
        $('#overlay').fadeOut(300);
         $('.calendar').fadeOut(300);
@@ -23,16 +27,10 @@ $(document).ready(function(){
    }); 
    
    $('#search').on('click', function(e){
+       // Cette fonction est déclenchée lorsque l'utilisateur clique sur le bouton "Search rooms"
        $('.booking').addClass('is-sent');
        e.preventDefault();
+       // Afficher une alerte indiquant que le site est une démo
+       alert("Ce site est une démo. Vous ne pouvez pas effectuer de réservation réelle.");
    });
-   });	
-
-   // Sélectionnez le bouton de recherche par son ID
-const searchButton = document.getElementById('search');
-
-// Ajoutez un écouteur d'événements pour le clic sur le bouton
-searchButton.addEventListener('click', function() {
-    // Affichez une alerte indiquant que le site est une démo
-    alert("Ce site est une démo. Vous ne pouvez pas effectuer de réservation réelle.");
 });

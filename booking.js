7035ce6b75fb768474108a4221b58335fd52c9bb
galathe <galathe.pro@yahoo.com>
2024-03-20 16:25:38 +0100

    dndn

--- booking.js
+++ booking.js
@@ -7,24 +7,23 @@ $(document).ready(function(){
     $('.pop-up').on('click', function(){
         $('#overlay').fadeIn(300); 
         $('.calendar').fadeIn(300); 
-        let clickedbutton = $("input",$(this).parent()).attr('id');
-        $('.dates').data('type',clickedbutton);
+        let clickedbutton = $(this).prev().attr('id'); // Récupérer l'ID de l'input précédent
+        $('.dates').data('type', clickedbutton);
     });
     
     $('table').on('click', function(event){
         let that=$(event.target);
         if(that.is('td') && !that.hasClass('notCurMonth') && !that.hasClass('holiday') && !that.hasClass('curDay')){
-            $('td.curDay').toggleClass('curDay');
-            that.toggleClass('curDay');
+            $('td.curDay').removeClass('curDay'); // Supprimer la classe curDay de l'ancien jour sélectionné
+            that.addClass('curDay'); // Ajouter la classe curDay au jour sélectionné
         }
     }); 
     
     $('#add_event').on('click', function(){
-        let value= $('td.curDay').html();
-        $('#overlay').fadeOut(300);
-        $('.calendar').fadeOut(300);
-        let id=($('.dates').data()).type;
-        $('#' + id).val(value+" May, 2014");
+        let value = $('td.curDay').html();
+        $('#overlay, .calendar').fadeOut(300); // Masquer l'overlay et le calendrier
+        let id = $('.dates').data('type');
+        $('#' + id).val(value + " May, 2014");
     }); 
     
     // Au clic sur le bouton "Search rooms"

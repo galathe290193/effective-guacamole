6b20fdb25f7f4314707cd134165dd1f7ed4d2b08
galathe <galathe.pro@yahoo.com>
2024-03-28 17:10:38 +0100

    song

--- music.js
+++ music.js
@@ -20,8 +20,39 @@ const songs = [
     source:
       "https://github.com/ecemgo/mini-samples-great-tricks/raw/main/song-list/Pawn-It-All.mp3",
   },
-  // ... (autres chansons)
+  {
+    title: "Seni Dert Etmeler",
+    name: "Madrigal",
+    source:
+      "https://github.com/ecemgo/mini-samples-great-tricks/raw/main/song-list/Madrigal-Seni-Dert-Etmeler.mp3",
+  },
+  {
+    title: "Instant Crush",
+    name: "Daft Punk ft. Julian Casablancas",
+    source:
+      "https://github.com/ecemgo/mini-samples-great-tricks/raw/main/song-list/Daft-Punk-Instant-Crush.mp3",
+  },
+  {
+    title: "As It Was",
+    name: "Harry Styles",
+    source:
+      "https://github.com/ecemgo/mini-samples-great-tricks/raw/main/song-list/Harry-Styles-As-It-Was.mp3",
+  },
+
+  {
+    title: "Physical",
+    name: "Dua Lipa",
+    source:
+      "https://github.com/ecemgo/mini-samples-great-tricks/raw/main/song-list/Dua-Lipa-Physical.mp3",
+  },
+  {
+    title: "Delicate",
+    name: "Taylor Swift",
+    source:
+      "https://github.com/ecemgo/mini-samples-great-tricks/raw/main/song-list/Taylor-Swift-Delicate.mp3",
+  },
 ];
+ 
 
 let currentSongIndex = 0; // Définir la première chanson comme chanson initiale
 

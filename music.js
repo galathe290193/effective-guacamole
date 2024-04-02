02aa2183486338321f9112e3c64ca24f62385601
galathe <galathe.pro@yahoo.com>
2024-04-02 03:46:17 +0200

    oldback

--- music.js
+++ music.js
@@ -1,212 +1,139 @@
-new Vue({
-	el: "#app",
-	data() {
-	  return {
-		audio: null,
-		circleLeft: null,
-		barWidth: null,
-		duration: null,
-		currentTime: null,
-		isTimerPlaying: false,
-		tracks: [
-		  {
-			name: "Mekanın Sahibi",
-			artist: "Norm Ender",
-			cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/1.jpg",
-			source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/1.mp3",
-			url: "https://www.youtube.com/watch?v=z3wAjJXbYzA",
-			favorited: false
-		  },
-		  {
-			name: "Everybody Knows",
-			artist: "Leonard Cohen",
-			cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/2.jpg",
-			source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/2.mp3",
-			url: "https://www.youtube.com/watch?v=Lin-a2lTelg",
-			favorited: true
-		  },
-		  {
-			name: "Extreme Ways",
-			artist: "Moby",
-			cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/3.jpg",
-			source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/3.mp3",
-			url: "https://www.youtube.com/watch?v=ICjyAe9S54c",
-			favorited: false
-		  },
-		  {
-			name: "Butterflies",
-			artist: "Sia",
-			cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/4.jpg",
-			source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/4.mp3",
-			url: "https://www.youtube.com/watch?v=kYgGwWYOd9Y",
-			favorited: false
-		  },
-		  {
-			name: "The Final Victory",
-			artist: "Haggard",
-			cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/5.jpg",
-			source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/5.mp3",
-			url: "https://www.youtube.com/watch?v=0WlpALnQdN8",
-			favorited: true
-		  },
-		  {
-			name: "Genius ft. Sia, Diplo, Labrinth",
-			artist: "LSD",
-			cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/6.jpg",
-			source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/6.mp3",
-			url: "https://www.youtube.com/watch?v=HhoATZ1Imtw",
-			favorited: false
-		  },
-		  {
-			name: "The Comeback Kid",
-			artist: "Lindi Ortega",
-			cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/7.jpg",
-			source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/7.mp3",
-			url: "https://www.youtube.com/watch?v=me6aoX0wCV8",
-			favorited: true
-		  },
-		  {
-			name: "Overdose",
-			artist: "Grandson",
-			cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/8.jpg",
-			source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/8.mp3",
-			url: "https://www.youtube.com/watch?v=00-Rl3Jlx-o",
-			favorited: false
-		  },
-		  {
-			name: "Rag'n'Bone Man",
-			artist: "Human",
-			cover: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/img/9.jpg",
-			source: "https://raw.githubusercontent.com/muhammederdem/mini-player/master/mp3/9.mp3",
-			url: "https://www.youtube.com/watch?v=L3wKzyIN1yk",
-			favorited: false
-		  }
-		],
-		currentTrack: null,
-		currentTrackIndex: 0,
-		transitionName: null
-	  };
-	},
-	methods: {
-	  play() {
-		if (this.audio.paused) {
-		  this.audio.play();
-		  this.isTimerPlaying = true;
-		} else {
-		  this.audio.pause();
-		  this.isTimerPlaying = false;
-		}
-	  },
-	  generateTime() {
-		let width = (100 / this.audio.duration) * this.audio.currentTime;
-		this.barWidth = width + "%";
-		this.circleLeft = width + "%";
-		let durmin = Math.floor(this.audio.duration / 60);
-		let dursec = Math.floor(this.audio.duration - durmin * 60);
-		let curmin = Math.floor(this.audio.currentTime / 60);
-		let cursec = Math.floor(this.audio.currentTime - curmin * 60);
-		if (durmin < 10) {
-		  durmin = "0" + durmin;
-		}
-		if (dursec < 10) {
-		  dursec = "0" + dursec;
-		}
-		if (curmin < 10) {
-		  curmin = "0" + curmin;
-		}
-		if (cursec < 10) {
-		  cursec = "0" + cursec;
-		}
-		this.duration = durmin + ":" + dursec;
-		this.currentTime = curmin + ":" + cursec;
-	  },
-	  updateBar(x) {
-		let progress = this.$refs.progress;
-		let maxduration = this.audio.duration;
-		let position = x - progress.offsetLeft;
-		let percentage = (100 * position) / progress.offsetWidth;
-		if (percentage > 100) {
-		  percentage = 100;
-		}
-		if (percentage < 0) {
-		  percentage = 0;
-		}
-		this.barWidth = percentage + "%";
-		this.circleLeft = percentage + "%";
-		this.audio.currentTime = (maxduration * percentage) / 100;
-		this.audio.play();
-	  },
-	  clickProgress(e) {
-		this.isTimerPlaying = true;
-		this.audio.pause();
-		this.updateBar(e.pageX);
-	  },
-	  prevTrack() {
-		this.transitionName = "scale-in";
-		this.isShowCover = false;
-		if (this.currentTrackIndex > 0) {
-		  this.currentTrackIndex--;
-		} else {
-		  this.currentTrackIndex = this.tracks.length - 1;
-		}
-		this.currentTrack = this.tracks[this.currentTrackIndex];
-		this.resetPlayer();
-	  },
-	  nextTrack() {
-		this.transitionName = "scale-out";
-		this.isShowCover = false;
-		if (this.currentTrackIndex < this.tracks.length - 1) {
-		  this.currentTrackIndex++;
-		} else {
-		  this.currentTrackIndex = 0;
-		}
-		this.currentTrack = this.tracks[this.currentTrackIndex];
-		this.resetPlayer();
-	  },
-	  resetPlayer() {
-		this.barWidth = 0;
-		this.circleLeft = 0;
-		this.audio.currentTime = 0;
-		this.audio.src = this.currentTrack.source;
-		setTimeout(() => {
-		  if(this.isTimerPlaying) {
-			this.audio.play();
-		  } else {
-			this.audio.pause();
-		  }
-		}, 300);
-	  },
-	  favorite() {
-		this.tracks[this.currentTrackIndex].favorited = !this.tracks[
-		  this.currentTrackIndex
-		].favorited;
-	  }
-	},
-	created() {
-	  let vm = this;
-	  this.currentTrack = this.tracks[0];
-	  this.audio = new Audio();
-	  this.audio.src = this.currentTrack.source;
-	  this.audio.ontimeupdate = function() {
-		vm.generateTime();
-	  };
-	  this.audio.onloadedmetadata = function() {
-		vm.generateTime();
-	  };
-	  this.audio.onended = function() {
-		vm.nextTrack();
-		this.isTimerPlaying = true;
-	  };
-  
-	  // this is optional (for preload covers)
-	  for (let index = 0; index < this.tracks.length; index++) {
-		const element = this.tracks[index];
-		let link = document.createElement('link');
-		link.rel = "prefetch";
-		link.href = element.cover;
-		link.as = "image"
-		document.head.appendChild(link)
-	  }
-	}
-  });
-  
+const progress = document.getElementById("progress");
+const song = document.getElementById("song");
+const controlIcon = document.getElementById("controlIcon");
+const playPauseButton = document.querySelector(".play-pause-btn");
+const forwardButton = document.querySelector(".controls button.forward");
+const backwardButton = document.querySelector(".controls button.backward");
+const songName = document.querySelector(".music-player h1");
+const artistName = document.querySelector(".music-player p");
+
+const songs = [
+  {
+    title: "Symphony",
+    name: "Clean Bandit ft. Zara Larsson",
+    source:
+      "https://github.com/ecemgo/mini-samples-great-tricks/raw/main/song-list/Clean-Bandit-Symphony.mp3",
+  },
+  {
+    title: "Pawn It All",
+    name: "Alicia Keys",
+    source:
+      "https://github.com/ecemgo/mini-samples-great-tricks/raw/main/song-list/Pawn-It-All.mp3",
+  },
+  {
+    title: "Seni Dert Etmeler",
+    name: "Madrigal",
+    source:
+      "https://github.com/ecemgo/mini-samples-great-tricks/raw/main/song-list/Madrigal-Seni-Dert-Etmeler.mp3",
+  },
+  {
+    title: "Instant Crush",
+    name: "Daft Punk ft. Julian Casablancas",
+    source:
+      "https://github.com/ecemgo/mini-samples-great-tricks/raw/main/song-list/Daft-Punk-Instant-Crush.mp3",
+  },
+  {
+    title: "As It Was",
+    name: "Harry Styles",
+    source:
+      "https://github.com/ecemgo/mini-samples-great-tricks/raw/main/song-list/Harry-Styles-As-It-Was.mp3",
+  },
+
+  {
+    title: "Physical",
+    name: "Dua Lipa",
+    source:
+      "https://github.com/ecemgo/mini-samples-great-tricks/raw/main/song-list/Dua-Lipa-Physical.mp3",
+  },
+  {
+    title: "Delicate",
+    name: "Taylor Swift",
+    source:
+      "https://github.com/ecemgo/mini-samples-great-tricks/raw/main/song-list/Taylor-Swift-Delicate.mp3",
+  },
+];
+
+let currentSongIndex = 3;
+
+function updateSongInfo() {
+  songName.textContent = songs[currentSongIndex].title;
+  artistName.textContent = songs[currentSongIndex].name;
+  song.src = songs[currentSongIndex].source;
+
+  song.addEventListener("loadeddata", function () {});
+}
+
+song.addEventListener("timeupdate", function () {
+  if (!song.paused) {
+    progress.value = song.currentTime;
+  }
+});
+
+song.addEventListener("loadedmetadata", function () {
+  progress.max = song.duration;
+  progress.value = song.currentTime;
+});
+
+function pauseSong() {
+  song.pause();
+  controlIcon.classList.remove("fa-pause");
+  controlIcon.classList.add("fa-play");
+}
+
+function playSong() {
+  song.play();
+  controlIcon.classList.add("fa-pause");
+  controlIcon.classList.remove("fa-play");
+}
+
+function playPause() {
+  if (song.paused) {
+    playSong();
+  } else {
+    pauseSong();
+  }
+}
+
+playPauseButton.addEventListener("click", playPause);
+
+progress.addEventListener("input", function () {
+  song.currentTime = progress.value;
+});
+
+progress.addEventListener("change", function () {
+  playSong();
+});
+
+forwardButton.addEventListener("click", function () {
+  currentSongIndex = (currentSongIndex + 1) % songs.length;
+  updateSongInfo();
+  playPause();
+});
+
+backwardButton.addEventListener("click", function () {
+  currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length;
+  updateSongInfo();
+  playPause();
+});
+
+updateSongInfo();
+
+var swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  centeredSlides: true,
+  initialSlide: 3,
+  slidesPerView: "auto",
+  allowTouchMove: false,
+  spaceBetween: 40,
+  coverflowEffect: {
+    rotate: 25,
+    stretch: 0,
+    depth: 50,
+    modifier: 1,
+    slideShadows: false,
+  },
+  navigation: {
+    nextEl: ".forward",
+    prevEl: ".backward",
+  },
+});

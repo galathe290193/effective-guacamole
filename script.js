34be275b35a5ef4a0177c85898af4330b713e215
galathe <galathe.pro@yahoo.com>
2024-04-13 10:16:27 +0200

    snnn

--- script.js
+++ script.js
@@ -1,42 +1,22 @@
-var theToggle = document.getElementById('toggle');
-
-// based on Todd Motto functions
-// https://toddmotto.com/labs/reusable-js/
-
-// hasClass
-function hasClass(elem, className) {
-	return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
-}
-// addClass
-function addClass(elem, className) {
-    if (!hasClass(elem, className)) {
-    	elem.className += ' ' + className;
-    }
-}
-// removeClass
-function removeClass(elem, className) {
-	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
-	if (hasClass(elem, className)) {
-        while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
-            newClass = newClass.replace(' ' + className + ' ', ' ');
-        }
-        elem.className = newClass.replace(/^\s+|\s+$/g, '');
-    }
-}
-// toggleClass
-function toggleClass(elem, className) {
-	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, " " ) + ' ';
-    if (hasClass(elem, className)) {
-        while (newClass.indexOf(" " + className + " ") >= 0 ) {
-            newClass = newClass.replace( " " + className + " " , " " );
-        }
-        elem.className = newClass.replace(/^\s+|\s+$/g, '');
-    } else {
-        elem.className += ' ' + className;
-    }
-}
-
-theToggle.onclick = function() {
-   toggleClass(this, 'on');
-   return false;
-}
+document.addEventListener("DOMContentLoaded", function () {
+    const menuToggle = document.getElementById("menu-toggle");
+    const homeModal = document.getElementById("home-modal");
+    const aboutModal = document.getElementById("about-modal");
+    const contactModal = document.getElementById("contact-modal");
+  
+    menuToggle.addEventListener("click", function () {
+      homeModal.style.display = "block";
+    });
+  
+    homeModal.addEventListener("click", function () {
+      homeModal.style.display = "none";
+    });
+  
+    aboutModal.addEventListener("click", function () {
+      aboutModal.style.display = "none";
+    });
+  
+    contactModal.addEventListener("click", function () {
+      contactModal.style.display = "none";
+    });
+  });

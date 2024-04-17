1bcddd7690f6ad9c8c6ddb12f50f0bd81d51385f
galathe <galathe.pro@yahoo.com>
2024-04-17 15:49:34 +0200

    nnn

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function closeModal(e) {
     if (e.target === this || e.target.classList.contains("btn-close")) {
-      const activeModal = document.querySelector(".modal.display-block");
+      const activeModal = document.querySelector(".modal");
       activeModal.style.display = "none";
     }
   }

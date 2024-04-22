e3ca86ed16b3873a052fd771e54b4253414b71c7
galathe <galathe.pro@yahoo.com>
2024-04-22 17:20:27 +0200

    toogle responsive

--- script.js
+++ script.js
@@ -1,55 +1,64 @@
 var theToggle = document.getElementById('toggle');
 var theMenu = document.getElementById('menu');
 
-// based on Todd Motto functions
-// https://toddmotto.com/labs/reusable-js/
-
 // hasClass
 function hasClass(elem, className) {
-	return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
-}
-// addClass
-function addClass(elem, className) {
-    if (!hasClass(elem, className)) {
-    	elem.className += ' ' + className;
-    }
-}
-// removeClass
-function removeClass(elem, className) {
-	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
-	if (hasClass(elem, className)) {
-        while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
-            newClass = newClass.replace(' ' + className + ' ', ' ');
-        }
-        elem.className = newClass.replace(/^\s+|\s+$/g, '');
-    }
-}
-// toggleClass
-function toggleClass(elem, className) {
-	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, " " ) + ' ';
-    if (hasClass(elem, className)) {
-        while (newClass.indexOf(" " + className + " ") >= 0 ) {
-            newClass = newClass.replace( " " + className + " " , " " );
-        }
-        elem.className = newClass.replace(/^\s+|\s+$/g, '');
-    } else {
-        elem.className += ' ' + className;
-    }
+  return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
 }
 
-theToggle.onclick = function() {
-   toggleClass(this, 'on');
-   adjustMenuPosition();
-   return false;
+// addClass
+function addClass(elem, className) {
+  if (!hasClass(elem, className)) {
+    elem.className += ' ' + className;
+  }
+}
+
+// removeClass
+function removeClass(elem, className) {
+  var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
+  if (hasClass(elem, className)) {
+    while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+      newClass = newClass.replace(' ' + className + ' ', ' ');
+    }
+    elem.className = newClass.replace(/^\s+|\s+$/g, '');
+  }
+}
+
+// toggleClass
+function toggleClass(elem, className) {
+  var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, " " ) + ' ';
+  if (hasClass(elem, className)) {
+    while (newClass.indexOf(" " + className + " ") >= 0 ) {
+      newClass = newClass.replace( " " + className + " " , " " );
+    }
+    elem.className = newClass.replace(/^\s+|\s+$/g, '');
+  } else {
+    elem.className += ' ' + className;
+  }
+}
+
+theToggle.addEventListener('click', function() {
+  toggleClass(this, 'on');
+  toggleMenu();
+});
+
+function toggleMenu() {
+  if (hasClass(theToggle, 'on')) {
+    theMenu.style.opacity = 1;
+    theMenu.style.visibility = 'visible';
+  } else {
+    theMenu.style.opacity = 0;
+    theMenu.style.visibility = 'hidden';
+  }
 }
 
 // Function to set toggle position
 function setTogglePosition() {
   var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
   if (scrollTop > 20) { 
-      theToggle.style.top = (scrollTop + 20) + 'px';
+    theToggle.style.top = (scrollTop + 20) + 'px';
   } else {
-      theToggle.style.top = '20px';
+    theToggle.style.top = '20px';
   }
 }
 
@@ -59,69 +68,16 @@ function adjustMenuPosition() {
   theMenu.style.top = topPosition + 'px';
 }
 
-// Appel de la fonction pour la première fois
+// Initial call
 setTogglePosition();
 
-// Gérer le scroll pour ajuster la position du toggle
+// Handle scroll to adjust toggle position
 window.addEventListener('scroll', setTogglePosition);
 
-document.addEventListener('DOMContentLoaded', function () {
-    var menuLinks = document.querySelectorAll('#menu li a');
-    var homeModal = document.getElementById('home-modal');
-    var aboutModal = document.getElementById('about-modal');
-    var contactModal = document.getElementById('contact-modal');
-    var modals = document.querySelectorAll('.modal');
-
-    // Function to close all modals
-    function closeModal() {
-        modals.forEach(function (modal) {
-            modal.style.display = 'none';
-        });
-    }
-
-    // Open Home Modal
-    menuLinks[0].addEventListener('click', function (e) {
-        e.preventDefault();
-        closeModal();
-        homeModal.style.display = 'block';
-    });
-
-    // Open About Modal
-    menuLinks[1].addEventListener('click', function (e) {
-        e.preventDefault();
-        closeModal();
-        aboutModal.style.display = 'block';
-    });
-
-    // Open Contact Modal
-    menuLinks[2].addEventListener('click', function (e) {
-        e.preventDefault();
-        closeModal();
-        contactModal.style.display = 'block';
-    });
-
-    // Close modal on button click
-    var closeButtons = document.querySelectorAll('.btn-close');
-    closeButtons.forEach(function (btn) {
-        btn.addEventListener('click', function () {
-            closeModal();
-        });
-    });
-
-    // Close modal on outside click
-    modals.forEach(function (modal) {
-        modal.addEventListener('click', function (e) {
-            if (e.target === modal) {
-                closeModal();
-            }
-        });
-    });
-});
-
+// Close menu on outside click
 document.addEventListener('click', function(e) {
-    if (!theMenu.contains(e.target) && !theToggle.contains(e.target)) {
-      removeClass(theToggle, 'on');
-      theMenu.style.opacity = 0;
-      theMenu.style.visibility = 'hidden';
-    }
-  });
+  if (!theMenu.contains(e.target) && !theToggle.contains(e.target)) {
+    removeClass(theToggle, 'on');
+    toggleMenu();
+  }
+});
